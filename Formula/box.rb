c0266e76e2922cdac00cbb4d9229027738a13cf6
Yusuke Shibata <yusukeshib@gmail.com>
2026-08-01 06:31:14 -0400

    box: let Homebrew infer version from URLs

--- Formula/box.rb
+++ Formula/box.rb
@@ -1,27 +1,26 @@
 class Box < Formula
   desc "Sandboxed git workspaces for development"
   homepage "https://github.com/yusukeshib/box"
-  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/yusukeshib/box/releases/download/v#{version}/box-aarch64-darwin"
+      url "https://github.com/yusukeshib/box/releases/download/v0.5.0/box-aarch64-darwin"
       sha256 "4a7dfa51d28a1bb47a17fa8da103cc35989ef7b1226d8adbfb07d5e27e5ab4a9"
     end
     on_intel do
-      url "https://github.com/yusukeshib/box/releases/download/v#{version}/box-x86_64-darwin"
+      url "https://github.com/yusukeshib/box/releases/download/v0.5.0/box-x86_64-darwin"
       sha256 "e8d696634190f2d73ed14cbb83721fbb4592c1e92380ad27fa41c3a573b16260"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/yusukeshib/box/releases/download/v#{version}/box-aarch64-linux"
+      url "https://github.com/yusukeshib/box/releases/download/v0.5.0/box-aarch64-linux"
       sha256 "1770eec973d31704b6e6b93e9570acb2b4dcf9b45f169f168f4d98725cbb5b7d"
     end
     on_intel do
-      url "https://github.com/yusukeshib/box/releases/download/v#{version}/box-x86_64-linux"
+      url "https://github.com/yusukeshib/box/releases/download/v0.5.0/box-x86_64-linux"
       sha256 "7e925bbbf35cee923869912604ae029fd6c88fa7bcb1f20d7f85918c4b023873"
     end
   end
